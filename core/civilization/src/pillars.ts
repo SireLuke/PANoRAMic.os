e21ff9216535f7edf72e97192b1a57ac2deb49d9
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 12:18:52 -0500

    Add CivilizationPillars constant with categories

---
+++ core/civilization/src/pillars.ts
@@ -0,0 +1,22 @@
+// core/civilization/src/pillars.ts
+
+export const CivilizationPillars = {
+    environmental: [
+        "pollution",
+        "emissions",
+        "waste",
+        "waterUse",
+        "landUse"
+    ],
+    humanRights: [
+        "violations",
+        "forcedMigration",
+        "trafficking",
+        "childTrafficking"
+    ],
+    governance: [
+        "corruption",
+        "laborViolations",
+        "governanceFailures"
+    ]
+};
