acec91b3fccf20cf13bb815b76316fa2aa3720ee
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 15:08:07 -0500

    Add dynamicFloat with dynamic functions

--- core/economy/src/index.ts
+++ core/economy/src/index.ts
@@ -1,5 +1,11 @@
 // core/economy/src/index.ts
+import { computeDynamicParCap } from "./dynamicFloat";
+import { canIssueParDynamic } from "./digitalIssuance";
 
+export const dynamicFloat = {
+    cap: computeDynamicParCap,
+    canIssue: canIssueParDynamic
+};
 import { convertPARToLocal } from "./currencyConversion";
 import { computeRealAssetValue } from "./realAssetEquivalence";
 import { computeNationAdjustment } from "./nationFactors";
