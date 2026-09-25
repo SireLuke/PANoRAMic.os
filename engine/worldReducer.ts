2362ee93f01cb3d07bf0f5b78765f08c53001d2c
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 16:39:26 -0500

    Implement worldReducer to manage world state updates

---
+++ engine/worldReducer.ts
@@ -0,0 +1,29 @@
+// engine/worldReducer.ts
+
+import { WorldState } from "./worldState";
+
+// Subsystem imports (append more as they come online)
+import { applyMetabolism } from "./metabolism";
+import { applyStability } from "./stability";
+import { applyCatastrophe } from "./catastrophe";
+import { applyDampening } from "./dampening";
+import { applySynthesis } from "./synthesis";
+import { applyEvents } from "./events";
+import { applyRenewables } from "./renewables";
+import { applyIncentives } from "./incentives";
+
+export function reduceWorld(world: WorldState): WorldState {
+    let next = { ...world };
+
+    // Apply subsystem updates in logical order
+    next = applyMetabolism(next);
+    next = applyRenewables(next);
+    next = applyIncentives(next);
+    next = applyCatastrophe(next);
+    next = applyDampening(next);
+    next = applyStability(next);
+    next = applyEvents(next);
+    next = applySynthesis(next);
+
+    return next;
+}
