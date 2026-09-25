739100a4165c4764b41fc3817ad9f5118b5004fd
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 00:32:07 -0500

    Create poi.ts

---
+++ core/travel/src/poi.ts
@@ -0,0 +1,12 @@
+export function poi() {
+    return {
+        status: "ready",
+        types: [
+            "infrastructure",
+            "renewables",
+            "humanitarian",
+            "education",
+            "catastrophe"
+        ]
+    };
+}
