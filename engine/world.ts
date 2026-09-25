eef1b57cd28ec0a75bed357f9cad7e103d4f6caa
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-24 22:44:04 -0500

    Create world.ts

---
+++ engine/world.ts
@@ -0,0 +1,11 @@
+export const pipeline = [
+    "metabolism",
+    "global",
+    "humanitarian",
+    "education",
+    "stability",
+    "synthesis",
+    "catastrophe",
+    "dampening",
+    "events"
+];
