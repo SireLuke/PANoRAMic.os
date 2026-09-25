3426e012e34cb60321a39e6b08cf5bc097e075d1
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 00:40:24 -0500

    Update run.ts

--- engine/run.ts
+++ engine/run.ts
@@ -1,3 +1,8 @@
+import { onboardingIncentive } from "./incentive.js";
+
 export function run(world, subsystems) {
+    // run incentive logic
+    world.incentive = onboardingIncentive(world.population, world.projectFraction);
+
     return world;
 }
