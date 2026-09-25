877090f87f45790a1ff6044921c2ca796ff3625b
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 07:17:04 -0500

    Add travel function to index.ts

---
+++ core/index.ts
@@ -0,0 +1,10 @@
+export function travel() {
+    return {
+        status: "ready",
+        mobility: "ready",
+        friction: "ready",
+        access: "ready",
+        cost: "ready",
+        risk: "ready"
+    };
+}
