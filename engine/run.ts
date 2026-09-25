7403b264018183e1fe0ebdb2a9315ead7b15e9aa
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 00:50:03 -0500

    Update run.ts

--- engine/run.ts
+++ engine/run.ts
@@ -1,11 +1,20 @@
 import { travel } from "../core/travel/src/index.js";
 import { poi } from "../core/travel/src/poi.js";
 import { onboardingIncentive } from "./incentive.js";
-import { onboardingIncentive } from "./incentive.js";
 
 export function run(world, subsystems) {
-    // run incentive logic
-    world.incentive = onboardingIncentive(world.population, world.projectFraction);
+
+    // Travel subsystem
+    world.travel = travel();
+
+    // POI subsystem
+    world.poi = poi();
+
+    // PAR onboarding incentive
+    world.incentive = onboardingIncentive(
+        world.population,
+        world.projectFraction
+    );
 
     return world;
 }
