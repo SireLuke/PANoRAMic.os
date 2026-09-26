a8601f532cc776363f47c6801db7388ae4144b41
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 21:37:37 -0500

    Create index.ts to export command modules

---
+++ cli/src/index.ts
@@ -0,0 +1,7 @@
+// cli/src/index.ts
+
+export { tickCommand } from "./commands/tick";
+export { statusCommand } from "./commands/status";
+export { dashboardCommand } from "./commands/dashboard";
+export { ramsCommand } from "./commands/rams";
+export { signalsCommand } from "./commands/signals";
