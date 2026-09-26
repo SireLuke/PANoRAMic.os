d8a9e98b695e77b811e66061ae9a725ee83ce18a
SIreluke <332733819+SireLuke@users.noreply.github.com>
2026-09-25 22:16:32 -0500

    Add searchCommand and loadConfig exports

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -5,3 +5,5 @@ export { statusCommand } from "./commands/status";
 export { dashboardCommand } from "./commands/dashboard";
 export { ramsCommand } from "./commands/rams";
 export { signalsCommand } from "./commands/signals";
+export { searchCommand } from "./commands/search";
+export { loadConfig } from "./commands/config";
